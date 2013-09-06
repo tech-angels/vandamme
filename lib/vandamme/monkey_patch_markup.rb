cc312868d1b8ce64554803e7d49957a0b38012d2
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2013-09-06 07:27:14 +0000

    Monkey patch for ruby 2.0
    Remove this file when https://github.com/github/markup/pull/214 is
    merged.

---
+++ lib/vandamme/monkey_patch_markup.rb
@@ -0,0 +1,17 @@
+# https://github.com/github/markup/pull/214
+# require "github/markup/rdoc"
+
+module GitHub
+    module Markup
+          class RDoc
+            def to_html
+              if Gem::Version.new(::RDoc::VERSION) < Gem::Version.new('4.0.0')
+                h = ::RDoc::Markup::ToHtml.new
+              else
+                h = ::RDoc::Markup::ToHtml.new(::RDoc::Options.new)
+              end
+              h.convert(@content)
+            end
+          end
+    end
+end
